a4ce349ba1edb8ddded9f1e8bee91539d848fe91
Mike Hewitt <MatrixMike@users.noreply.github.com>
2019-05-05 23:02:10 +1000

    Update Display_print.ino

--- examples/Display_print/Display_print.ino
+++ examples/Display_print/Display_print.ino
@@ -8,7 +8,7 @@
 #include <Diablo_Serial_4DLib.h>
 
 //use Serial0 to communicate with the display.
-Diablo_Serial_4DLib Display(&DisplaySerial); 
+Diablo_Serial_4DLib Display(&DisplaySerial);
 
 //---------END-----------------
 
@@ -16,23 +16,23 @@ void setup() {
   //For handling errors
   Display.Callback4D = mycallback ;
 
-  //5 second timeout on all commands  
+  //5 second timeout on all commands
   Display.TimeLimit4D   = 5000 ;
-  
+
   DisplaySerial.begin(9600) ;
 
   //--------------------------------Optional reset routine-----------------------------------
   //Reset the Display using D4 of the Arduino (if using the new 4D Arduino Adaptor - Rev 2)
   //If using the old 4D Arduino Adaptor (Rev 1), change D4 to D2 below.
-  //If using jumper wires, reverse the logic states below. 
+  //If using jumper wires, reverse the logic states below.
   //Refer to the accompanying application note for important information.
   pinMode(4, OUTPUT);  // Set D4 on Arduino to Output (4D Arduino Adaptor V2 - Display Reset)
   digitalWrite(4, 1);  // Reset the Display via D4
   delay(100);
   digitalWrite(4, 0);  // unReset the Display via D4
   //-----------------------------------------END---------------------------------------------
-  
-  delay (5000); //let the display start up  
+
+  delay (5000); //let the display start up
 
   Display.gfx_ScreenMode(PORTRAIT);
   //Display.gfx_BGcolour(WHITE) ; //change background color to white
@@ -51,33 +51,33 @@ void setup() {
   float nan = NAN;
   float inf = INFINITY;
   float ovf1 = 5294967040.0;
-  float ovf2 = -5294967040.0; 
-  uint32_t e = 123 ;  
+  float ovf2 = -5294967040.0;
+  uint32_t e = 123 ;
   unsigned long e2 = 123 ;
 
-  
+
   Display.print(a); Display.print(' ');
   Display.println(a2);
   Display.print(b); Display.print(' ');
   Display.println(b2);
   Display.print(c); Display.print(' ');
   Display.println(c2);
-  Display.println("This is String"); 
-  Display.println(F("This is Flashstring"));  // COMMENT OUT FOR ARDUINO DUE, NOT SUPPORTED 
-  Display.println(string); 
+  Display.println("This is String");
+  Display.println(F("This is Flashstring"));  // COMMENT OUT FOR ARDUINO DUE, NOT SUPPORTED
+  Display.println(string);
   Display.println(charr);
-  Display.println(d,6);
-  Display.println(d2, 3);  
+  Display.println(d, 6);
+  Display.println(d2, 3);
   Display.println(nan);
-  Display.println(inf);  
+  Display.println(inf);
   Display.println(ovf1);
-  Display.println(ovf2);  
-  Display.println(e2); 
-  Display.print(e); 
+  Display.println(ovf2);
+  Display.println(e2);
+  Display.print(e);
 
 }
 
-void loop() 
+void loop()
 {
 
 }
@@ -87,7 +87,7 @@ void mycallback(int ErrCode, unsigned char Errorbyte)
   // Pin 13 has an LED connected on most Arduino boards. Just give it a name
   int led = 13;
   pinMode(led, OUTPUT);
-  while(1)
+  while (1)
   {
     digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(200);                // wait for 200 ms
